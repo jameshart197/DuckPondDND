0137beee6441dae365fce7aaa6131812c745c7dc
James Hart <jamesmurphy197197@gmail.com>
2022-11-14 16:48:16 +0000

    [FTR] Implement injection for cities in Xandros.

--- assets/scripts/city-content.js
+++ assets/scripts/city-content.js
@@ -1,6 +1,18 @@
 const mainElement = document.getElementsByTagName('main')[0];
+const titleElement = document.getElementsByTagName('title')[0];
 
-let worldtype = 
+let params = new URLSearchParams(window.location.search);
+let worldtype = params.get('worldtype');
+let worldname = params.get('worldname');
+let cityname = params.get('cityname');
 let url=`/Worlds/${worldtype}/${worldname}/cities/${cityname}.html`;
 
-fetch()
+fetch(url).then((response)=> {
+    return response.text();
+}).then((html)=>{
+    mainElement.innerHTML = html;
+    titleElement.innerText = `${worldname} | ${cityname}`;
+}).catch((err)=> {
+    console.warn('Something went wrong.', err);
+    mainElement.innerHTML = '';
+})
