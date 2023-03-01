855a8b4946d437bcc89dae46de57101408f9a3cf
James Hart <jamesmurphy197197@gmail.com>
2023-03-01 00:18:38 +0000

    [FTR] Make images clickable on tiles and style tiles

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -5,3 +5,11 @@ const footerElement = document.getElementsByTagName("footer")[0];
 
 fetchHeader(headerElement);
 fetchFooter(footerElement);
+
+var tiles = [...document.querySelectorAll(".tiled-list li")];
+// tiles.forEach(li => li.addEventListener("click", e => window.location.href = e.currentTarget.dataset.linkUrl))
+document.body.addEventListener("click", e => {
+    if (e.target.closest('.tiled-list') && e.target.tagName === "LI"){
+        window.location.href = e.target.dataset.linkUrl
+    }
+})
